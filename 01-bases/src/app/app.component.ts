7998ee9b557650ac6e8be3080004df6fd4cd557b
AbigailMRod <abigail.mrodriguez21@gmail.com>
2022-02-09 12:49:56 -0600

    Módulos y componentes en la sección de Introducción a Angular del curso de Angular en Udemy

--- 01-bases/src/app/app.component.ts
+++ 01-bases/src/app/app.component.ts
@@ -2,15 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title: string = 'Contador App';
-  number: number =10;
-
-
-  acumular(valor:number){
-    this.number += valor;
-  }
-
+  
 }
