338051ed86f51f6fa3e9723149aa4f1ea5821d17
AbigailMRod <abigail.mrodriguez21@gmail.com>
2022-02-15 15:29:54 -0600

    Término de la sección 8: SPA- paises app. En el curso de Angular de Udemy

--- 03-paisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -6,11 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent   {
 
-  constructor() { }
+  termino:string='';
+  hayError: boolean= false;
+  capitales: Country[]=[];
 
-  ngOnInit(): void {
+  constructor(private capitalService: PaisService) { }
+
+  buscar(termino:string){
+    this.hayError=false;
+    this.termino= termino;
+
+    this.capitalService.buscarCapital(termino)
+    .subscribe((capitales) =>{
+      console.log(capitales);
+      this.capitales=capitales;
+
+
+    }, (err) =>{
+      this.hayError= true;
+      this.capitales=[];
+    });
   }
+
 
 }
--- 03-paisesApp/src/app/pais/services/pais.service.ts
+++ 03-paisesApp/src/app/pais/services/pais.service.ts
@@ -9,7 +9,7 @@ import { Country } from '../interfaces/pais.interface';
 export class PaisService {
 
 
-  private apiURL: string= 'https://restcountries.com/v3.1';
+  private apiURL: string= 'https://restcountries.com/v2';
   
   
   constructor( private http: HttpClient) {   }
@@ -17,10 +17,20 @@ export class PaisService {
   buscarPais(termino: string): Observable<Country[]>{
     
     const url= `${this.apiURL}/name/${termino}`;
+    return this.http.get<Country[]>(url); 
+  }
+
+  buscarCapital(termino: string): Observable<Country[]>{
+    
+    const url= `${this.apiURL}/capital/${termino}`;
     return this.http.get<Country[]>(url);
     
   }
 
+  getPaisAlpha(id:string): Observable<Country>{
+    const url = `${this.apiURL}/alpha/${id}`;
+    return this.http.get<Country>(url);
+  }
 
 
 
