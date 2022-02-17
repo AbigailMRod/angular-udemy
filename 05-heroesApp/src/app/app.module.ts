4d40a64fc9cff1e0fdae0fd1f0d675b4bfdc3a2b
AbigailMRod <abigail.mrodriguez21@gmail.com>
2022-02-17 11:02:35 -0600

    Sección 12: Heroes app- Rutas hijas y Lazyload. Dentro del curso de Angular en Udemy

---
+++ 05-heroesApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ErrorPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
